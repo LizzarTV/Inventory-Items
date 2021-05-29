c8d1cad1a5ee392424ed416d1cfe29cdbe95788e
Thomas Kortyka <info@toxictoast.de>
2021-05-29 17:20:33 +0200

    update application handlers

--- src/application/commands/handlers/RestoreItem.handler.ts
+++ src/application/commands/handlers/RestoreItem.handler.ts
@@ -2,6 +2,8 @@ import {CommandHandler, EventPublisher, ICommandHandler} from "@nestjs/cqrs";
 import {AppFactory} from "../../../domain/factories/app.factory";
 import {RestoreItemCommand} from "../impl/RestoreItem.command";
 import {AppRepository} from "../../../infrastructure/repositories/app.repository";
+import {RpcException} from "@nestjs/microservices";
+import {HttpStatus} from "@nestjs/common";
 
 @CommandHandler(RestoreItemCommand)
 export class RestoreItemHandler implements ICommandHandler<RestoreItemCommand> {
@@ -15,9 +17,17 @@ export class RestoreItemHandler implements ICommandHandler<RestoreItemCommand> {
     public async execute(command: RestoreItemCommand): Promise<void> {
         const { id } = command;
         const model = await this.repository.findById(id);
-        const item = this.eventPublisher.mergeObjectContext(model);
-        item.restore();
-        item.commit();
-        await this.repository.save(item);
+        if (model !== null) {
+            const item = this.eventPublisher.mergeObjectContext(model);
+            item.restore();
+            item.commit();
+            await this.repository.save(item);
+        } else {
+            throw new RpcException({
+                code: HttpStatus.NOT_FOUND,
+                message: 'Entity not found',
+            });
+        }
+
     }
 }
